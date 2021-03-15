d2dfac9657bd0b79f3ece3aaa22d2162c4ffc361
Natalia_Konovalenko <natalia_konovalenko@epam.com>
2021-03-15 17:19:01 +0200

    custom hook for input reset

--- src/components/Modal/ModalContent/MovieForm/MovieForm.jsx
+++ src/components/Modal/ModalContent/MovieForm/MovieForm.jsx
@@ -1,12 +1,40 @@
-import React, {useContext} from "react";
+import React, {useState, useContext} from "react";
 import Button from "../../../Button/Button";
 import FormRow from "../../../Forms/FormRow";
 import ModalContext from "../../../../ModalContext";
+
+function useResetInput(initialValue) {
+    const[value, setValue] = useState(initialValue);
+
+    const onChange = e => {
+        setValue(e.target.value);
+    };
+
+    const reset = () => setValue('');
+
+    return {
+        value,
+        onChange,
+        reset
+    }
+}
 
 export default function MovieForm(props) {
     const {modalData} = useContext(ModalContext);
 
     const selectOptions = ['All', 'Documentary', 'Comedy', 'Horror', 'Crime'];
+
+    const inputTitle = useResetInput(modalData.title || '');
+    const inputMovieURL = useResetInput(modalData.url || '');
+    const inputOverview = useResetInput(modalData.overview || '');
+    const inputRuntime = useResetInput(modalData.runtime || '');
+
+    const handleFormReset = () => {
+        inputTitle.reset();
+        inputMovieURL.reset();
+        inputOverview.reset();
+        inputRuntime.reset();
+    };
 
     return(
         <form action="#" className="movie-form">
@@ -16,15 +44,17 @@ export default function MovieForm(props) {
                 ): "Add"} movie
             </h1>
             <FormRow label="Title">
-                <input id="Title" placeholder="Title" type="text" {...(props.isEditMovieForm && { defaultValue: modalData.title })} />
+                <input id="Title" placeholder="Title" type="text" onChange={inputTitle.onChange} value={inputTitle.value} />
             </FormRow>
             <FormRow label="Release date">
-                <input id="Release date" placeholder="Select date" type="date" {...(props.isEditMovieForm && { defaultValue: "Moana" })} />
+                {/*TODO update date*/}
+                <input id="Release date" placeholder="Select date" type="date" {...(props.isEditMovieForm && { defaultValue: "10/12/2021" })} />
             </FormRow>
             <FormRow label="Movie URL">
-                <input id="Movie URL" placeholder="Movie URL here" type="text" {...(props.isEditMovieForm && { defaultValue: "www.moana.com" })} />
+                <input id="Movie URL" placeholder="Movie URL here" type="text" onChange={inputMovieURL.onChange} value={inputMovieURL.value} />
             </FormRow>
             <FormRow label="Genre">
+                {/*TODO update select options*/}
                 <div className="select">
                     <select id="genre">
                         {selectOptions.map((option, index) => <option key={`option-${index}`} value={index}>{option}</option>)}
@@ -32,17 +62,17 @@ export default function MovieForm(props) {
                 </div>
             </FormRow>
             <FormRow label="Overview">
-                <input id="Overview" placeholder="Overview" type="text" {...(props.isEditMovieForm && { defaultValue: modalData.overview })} />
+                <input id="Overview" placeholder="Overview" type="text" onChange={inputOverview.onChange} value={inputOverview.value} />
             </FormRow>
             <FormRow label="Runtime">
-                <input id="Runtime" placeholder="Runtime" type="text" {...(props.isEditMovieForm && { defaultValue: modalData.runtime })} />
+                <input id="Runtime" placeholder="Runtime" type="text" onChange={inputRuntime.onChange} value={inputRuntime.value} />
             </FormRow>
             <div className="buttons-holder">
-                <Button className="btn-outline" title="reset" type="reset"/>
+                <Button type="button" className="btn-outline" title="reset" onButtonClick={handleFormReset} />
                 {props.isEditMovieForm ? (
                     <Button title="save" type="button"/>
                 ): <Button title="submit" type="submit"/> }
             </div>
         </form>
     )
-}
+};
