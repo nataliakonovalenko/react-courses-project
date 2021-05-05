3ddab8d0477a14dc06b9eb93aefd62a74f9509d0
Natalia_Konovalenko <natalia_konovalenko@epam.com>
2021-05-06 01:34:19 +0300

    update storybook

--- src/components/FilterList/FilterList.stories.js
+++ src/components/FilterList/FilterList.stories.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { withKnobs } from "@storybook/addon-knobs";
+import { text, withKnobs } from "@storybook/addon-knobs";
 import FilterList from "./FilterList";
+
+const shortid = require("shortid");
 
 export default {
     title: "FilterList",
@@ -8,15 +10,15 @@ export default {
     component: FilterList,
 };
 
-const filterArray = ["All", "Documentary", "Comedy", "Horror", "Crime"];
-
 export const List = () => {
+    const filterArray = ["All", "Documentary", "Comedy", "Horror", "Crime"];
+
     return (
         <ul className="filter-list">
-            {filterArray.map((filterLink) => {
+            {filterArray.map((filterLink, index) => {
                 return (
-                    <li>
-                        <a href="#">{filterLink}</a>
+                    <li key={shortid.generate()}>
+                        <a href="#">{text(`item${index}`, filterLink)}</a>
                     </li>
                 );
             })}
